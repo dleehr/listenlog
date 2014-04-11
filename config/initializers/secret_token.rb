baa08589636d41446d321a19fb49a4ce685e6b39
Dan Leehr <dan.leehr@nescent.org>
2014-04-11 13:42:28 -0400

    initial commit after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Listenlog::Application.config.secret_key_base = 'f4e5cfd0722ee5577195892c140a76d6e9e3e3bcefef606a5049892375c920701bfb668c6162a7c82f7e81ede5b6f5b2dd2794fbd3d8115b3dc56c3353c656ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Listenlog::Application.config.session_store :cookie_store, key: '_listenlog_session'
