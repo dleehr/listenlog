73b7f12711f827f34a02b59cd2a09a852bbdbffd
Dan Leehr <dan.leehr@nescent.org>
2014-04-11 16:46:09 -0400

    Made concert (from recording) a select

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,8 @@
 class Concert < ActiveRecord::Base
   has_many :recordings, :dependent => :destroy
   has_many :listen_events, :through => :recordings
+
+  def title
+    "#{performer} - #{date}"
+  end
 end
