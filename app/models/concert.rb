408b0d6fee539216e68fabd82700731c95d046a0
Dan Leehr <leehro@gmail.com>
2014-04-13 11:29:55 -0400

    Concert validates performer
    Fixes #5

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,6 +1,7 @@
 class Concert < ActiveRecord::Base
   has_many :recordings, :dependent => :destroy
   has_many :listen_events, :through => :recordings
+  validates :performer, :presence => true
 
   def title
     "#{performer} - #{date}"
--- test/models/concert_test.rb
+++ test/models/concert_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ConcertTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validates performer' do
+    concert = Concert.new()
+    assert_not concert.save, 'should not save without performer'
+  end
 end
