f7d5ff8116a98542dea38da9a8dc4e9f6dc3f29f
Dan Leehr <leehro@gmail.com>
2014-04-13 15:37:33 -0400

    Added scopes for start_events and finish_events

--- app/models/listen_event.rb
+++ app/models/listen_event.rb
@@ -3,6 +3,9 @@ class ListenEvent < ActiveRecord::Base
   FINISH = 2
   belongs_to :recording
   scope :by_recording, lambda{|r| where(:recording_id=> r)}
+  scope :start_events, lambda{ where :event_type => START }
+  scope :finish_events, lambda{ where :event_type => FINISH }
+
 
   def is_start?
     event_type == START
