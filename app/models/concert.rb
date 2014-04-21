e2baf4c41fde543adac48b964735ecd72258c38c
Dan Leehr <dan.leehr@nescent.org>
2014-04-21 10:06:57 -0400

    Change validation on concert from performer to artist

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,6 +2,7 @@ class Concert < ActiveRecord::Base
   has_many :recordings, :dependent => :destroy
   has_many :listen_events, :through => :recordings
   belongs_to :artist
+  validates_presence_of :artist
 
   def performer
     artist.name
