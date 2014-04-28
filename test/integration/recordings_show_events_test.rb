aedbea2ba40f8163a9aa1c7f956f657cbf1929bf
Dan Leehr <leehro@gmail.com>
2014-04-27 21:53:05 -0400

    Enhance RecordingsShowEventsTest with clicking link

--- test/integration/recordings_show_events_test.rb
+++ test/integration/recordings_show_events_test.rb
@@ -5,5 +5,8 @@ class RecordingsShowEventsTest < ActionDispatch::IntegrationTest
   test 'recordings show events' do
     visit(recording_path(recordings(:ob_la)))
     assert page.has_link? 'Events', "Page should have 'Events' link"
+    click_link 'Events'
+    print page.html
+    assert page.has_link? recording_path(recordings(:ob_la)), 'Unable to find link back to recording from events list""
   end
 end
