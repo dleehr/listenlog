86898ca47acc4ae33c5784cd730117aee8a50e64
Dan Leehr <leehro@gmail.com>
2014-12-19 18:07:53 -0500

    Switch to "Controller as " syntax
    Remove ng-route

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -2,9 +2,5 @@
 
 angular.module('listenlog', [
     'ngResource',
-    'ngRoute',
     'listenlog.current'
-]).config(['$routeProvider', function($routeProvider) {
-    console.log('configuring routeprovider');
-    $routeProvider.otherwise({redirectTo: '/current'});
-}]);
+]);
--- app/assets/javascripts/angular/controllers/current.js
+++ app/assets/javascripts/angular/controllers/current.js
@@ -1,13 +1,17 @@
 'use strict';
 
-angular.module('listenlog.current', ['ngRoute'])
-.config(['$routeProvider', function($routeProvider) {
-        console.log('configuring /current');
-        $routeProvider.when('/current', {
-            templateUrl: '/templates/current.html',
-            controller: 'CurrentCtrl'
-        });
-    }])
-.controller('CurrentCtrl', [function() {
-    console.log('current ctrl');
-    }]);
+angular.module('listenlog.current',[])
+.controller('CurrentController', CurrentController);
+
+function CurrentController() {
+    this.populateSamples();
+}
+
+CurrentController.prototype.populateSamples = function() {
+    this.show = "Philadelphia, PA";
+    this.recording = "Official Bootleg";
+    this.artist = "Pearl Jam";
+    this.date = "2000-09-01";
+    this.song = "Sometimes";
+};
+
