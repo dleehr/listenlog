65a15ec41b291978e48f53bd786fd3eb49485577
Dan Leehr <leehro@gmail.com>
2015-01-03 15:56:38 -0500

    Move moment to top, so defined for angular-moment

--- app/assets/javascripts/angular_bootstrap.js
+++ app/assets/javascripts/angular_bootstrap.js
@@ -1,11 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+//= require moment
 //= require angular
 //= require angular-animate
 //= require angular-resource
 //= require angular-ui-bootstrap
 //= require angular-ui-bootstrap-tpls
 //= require angular-moment.min
-//= require moment
 //= require_tree ./angular
