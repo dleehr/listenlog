416fc2dbc10488709cd6d5bbe77f632a72b74bd8
Dan Leehr <leehro@gmail.com>
2015-02-25 22:24:47 -0500

    Add missing filenames to asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Listenlog::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile = %w( angular_bootstrap.js angular_bootstrap.css )
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
